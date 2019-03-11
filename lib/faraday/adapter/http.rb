e7c340347350852da51bdf10e5c952ee79816b2e
Mattia <iMacTia@users.noreply.github.com>
2019-03-11 09:17:19 +0000

    Add HTTP.rb adapter (#1)

---
+++ lib/faraday/adapter/http.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Faraday
+  class Adapter
+    # HTTP.rb adapter.
+    class HTTP < Faraday::Adapter
+      # Takes the environment and performs the request.
+      #
+      # @param env [Faraday::Env] the request environment.
+      #
+      # @return [Faraday::Response] the response.
+      def call(env)
+        super
+        perform_request(env)
+        @app.call(env)
+      end
+
+      private
+
+      def perform_request(env)
+        conn = setup_connection(env)
+
+        resp = conn.request env[:method], env[:url], body: env[:body]
+        save_response(env, resp.code, resp.body.to_s, resp.headers, resp.status.reason)
+      rescue ::HTTP::TimeoutError
+        raise Faraday::TimeoutError, $ERROR_INFO
+      rescue ::HTTP::ConnectionError
+        raise Faraday::ConnectionFailed, $ERROR_INFO
+      end
+
+      def setup_connection(env)
+        conn = ::HTTP
+
+        request_config(conn, env[:request]) if env[:request]
+        conn.headers(env.request_headers)
+      end
+
+      def request_config(conn, config)
+        if (timeout = config[:timeout])
+          conn = conn.timeout(connect: timeout, read: timeout, write: timeout)
+        end
+
+        if (timeout = config[:open_timeout])
+          conn = conn.timeout(connect: timeout, write: timeout)
+        end
+
+        if (proxy = config[:proxy])
+          conn = conn.via(proxy.uri.host, proxy.uri.port, proxy.user, proxy.password)
+        end
+
+        conn
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage 95
+  minimum_coverage_by_file 80
+end
+
 require 'bundler/setup'
 require 'faraday/http'
+require 'faraday_specs_setup'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -13,4 +21,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.order = :random
+  Kernel.srand config.seed
 end
