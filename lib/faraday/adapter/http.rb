68dfbe2406d8e94f549d7c8f68575198fa7767d4
rick olson <technoweenie@gmail.com>
2019-04-25 16:31:51 -0600

    create custom SSL context from Faraday::SSLOptions

--- lib/faraday/adapter/http.rb
+++ lib/faraday/adapter/http.rb
@@ -4,6 +4,8 @@ module Faraday
   class Adapter
     # HTTP.rb adapter.
     class HTTP < Faraday::Adapter
+      dependency 'http'
+
       # Takes the environment and performs the request.
       #
       # @param env [Faraday::Env] the request environment.
@@ -20,7 +22,10 @@ module Faraday
       def perform_request(env)
         conn = setup_connection(env)
 
-        resp = conn.request env[:method], env[:url], body: env[:body]
+        resp = conn.request env[:method], env[:url],
+          body: env[:body],
+          ssl_context: ssl_context(env[:ssl])
+
         save_response(env, resp.code, resp.body.to_s, resp.headers, resp.status.reason)
       rescue ::HTTP::TimeoutError
         raise Faraday::TimeoutError, $ERROR_INFO
@@ -53,6 +58,66 @@ module Faraday
         end
 
         conn
+      end
+
+      def ssl_context(ssl)
+        params = {}
+        [
+          :ca_file, :ca_path, :cert_store, :verify_depth,
+        ].each do |key|
+          if (value = ssl[key])
+            params[key] = value
+          end
+        end
+
+        if (client_cert = ssl_client_cert(ssl.client_cert))
+          params[:cert] = client_cert
+        end
+
+        if (client_key = ssl_client_key(ssl.client_key))
+          params[:key] = client_key
+        end
+
+        OpenSSL::SSL::SSLContext.new.tap do |ctx|
+          ctx.set_params(params)
+          if (vm = ssl.verify_mode)
+            ctx.verify_mode = vm
+          elsif ssl.disable?
+            ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          end
+
+          if (v = ssl.version)
+            ctx.ssl_version = v
+          end
+
+          if ctx.respond_to?(:min_version=) && (v = ssl.min_version)
+            ctx.min_version = v
+          end
+        end
+      end
+
+      def ssl_client_cert(cert)
+        case cert
+        when NilClass then return
+        when String
+          return OpenSSL::X509::Certificate.new(File.read(cert))
+        when OpenSSL::X509::Certificate
+          return cert
+        else
+          raise Faraday::Error, "invalid ssl.client_cert: #{cert.inspect}"
+        end
+      end
+
+      def ssl_client_key(cert)
+        case cert
+        when NilClass then return
+        when String
+          return OpenSSL::PKey::RSA.new(File.read(cert))
+        when OpenSSL::PKey::RSA, OpenSSL::PKey::DSA
+          return cert
+        else
+          raise Faraday::Error, "invalid ssl.client_key: #{cert.inspect}"
+        end
       end
     end
   end
